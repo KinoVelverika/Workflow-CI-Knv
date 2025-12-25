4e2fa81310cde3e5bac0f571117c9c3371493146
KinoVelverika <reallyhappysoda@gmail.com>
2025-12-25 16:07:09 +0700

    Final Reset: New DagsHub Link

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -6,46 +6,49 @@ import mlflow
 import mlflow.sklearn
 import dagshub
 
-#connection setup
-dagshub.init(repo_owner='KinoVelverika', repo_name='Workflow-CI-knv', mlflow=True)
-mlflow.set_tracking_uri("https://dagshub.com/KinoVelverika/Workflow-CI-knv.mlflow")
-mlflow.set_experiment("Submission_Akhir_Timothy")
+#SETUP KONEKSI
+dagshub.init(repo_owner='KinoVelverika', repo_name='bangun-sistem-ML', mlflow=True)
+mlflow.set_tracking_uri("https://dagshub.com/KinoVelverika/bangun-sistem-ML.mlflow")
+mlflow.set_experiment("Submission_Final_Timothy")
 
 #PREPROCESSING
 print("Membaca data...")
 df = pd.read_csv('credit_risk_dataset.csv')
-
-
 df.dropna(inplace=True)
 
-# Tentukan Fitur (X) dan Target (y)
+# Bersihkan data kosong
+df.dropna(inplace=True)
+
+# Fitur (X)
 X = df[['income', 'age', 'loan_amount', 'credit_history']]
+# Target (y)
 y = df['approved']
 
+# Encode data kategori (ubah teks jadi angka)
 X = pd.get_dummies(X, drop_first=True)
 
-# membagi data latih dan uji
+# Split Data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-#Training dan Tracking
+#TRAINING & TRACKING
 estimators = [50, 100]
 
 for n in estimators:
     with mlflow.start_run(run_name=f"Model_RF_{n}"):
         print(f"Training dengan {n} pohon...")
-        
+
         # Latih Model
         model = RandomForestClassifier(n_estimators=n, random_state=42)
         model.fit(X_train, y_train)
-        
+
         # Evaluasi
         acc = accuracy_score(y_test, model.predict(X_test))
-        
+
         # Log ke DagsHub
         mlflow.log_param("n_estimators", n)
         mlflow.log_metric("accuracy", acc)
         mlflow.sklearn.log_model(model, "model_rf")
-        
+
         print(f"Selesai! Akurasi: {acc}")
 
 print("Semua proses selesai!")
